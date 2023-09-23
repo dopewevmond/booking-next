4c0dd7a3840b7a159fc8581cce6af8c15ab5405c
dopewevmond <dopewevmond@gmail.com>
2023-09-23 22:12:22 +0000

    buttons for easier nav

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default async function Home() {
   return (
@@ -15,6 +16,15 @@ export default async function Home() {
                 alt=''
                 className='w-full h-auto rounded-lg'
               />
+
+              <div className='flex gap-4 items-center'>
+                <Link href='/add-room' className='w-full text-white bg-orange-600 hover:bg-orange-700 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-800 flex items-center gap-2 justify-center'>
+                  Admin Portal
+                </Link>
+                <Link href='/book' className='w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 flex items-center gap-2 justify-center'>
+                  Book Room
+                </Link>
+              </div>
             </div>
           </div>
         </div>
