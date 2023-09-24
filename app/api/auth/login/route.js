2b446bc3a453ff7bb6fe6d194f81d1ce9bb1aa1f
dopewevmond <dopewevmond@gmail.com>
2023-09-24 00:35:49 +0000

    remove unnecessary logs

--- app/api/auth/login/route.js
+++ app/api/auth/login/route.js
@@ -9,16 +9,11 @@ export async function POST(req) {
     const { passwordHash, isAdmin: isadmin } = await User.findOne({ username });
     const isPasswordCorrect = await checkPasswordsMatch(password, passwordHash);
     
-    console.log('checkpoint 1')
     if (!isPasswordCorrect) {
       throw new Error('Passwords don\'t match');
     }
-
-    console.log('checkpoint 2')
     
     const jwt = await createJWT({ username, isadmin });
-    console.log({ jwt })
-    console.log('checkpoint 3')
   
     cookies().set({
       name: String(process.env.COOKIE_KEY),
